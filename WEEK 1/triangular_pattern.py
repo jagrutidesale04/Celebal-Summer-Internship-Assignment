8864c83850f433de0f7604dc1fc55760c2c0d22f
Jagruti Desale <147822711+jagrutidesale04@users.noreply.github.com>
2025-06-08 14:34:45 +0530

    Update triangular_pattern.py

--- WEEK 1/triangular_pattern.py
+++ WEEK 1/triangular_pattern.py
@@ -1,1 +1,15 @@
+print("\nWEEK 1 ASSIGNMENT")
 
+rows = int(input("Enter the number of rows for the triangle patterns: "))
+
+print("\nLower Triangular Pattern")
+for i in range(1, rows + 1):
+    print("*" * i)
+  
+print("\nUpper Triangular Pattern")
+for i in range(rows, 0, -1):
+    print(" " * (rows - i) + "*" * i)
+  
+print("\nPyramid Pattern")
+for i in range(1, rows + 1):
+    print(" " * (rows - i) + "*" * (2 * i - 1))
